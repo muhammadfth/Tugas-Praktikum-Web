70258e57a00d39424936eaba1de2341a5d6d1874
muhammadf4th <mhmmdf4th@gmail.com>
2023-10-31 09:55:35 +0700

    week-5

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\AnimalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+# mengimport controller Student
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
@@ -19,14 +21,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Method GET
+# Route animals
 Route::get('/animals', [AnimalController::class, 'index']);
-
-// Method POST
 Route::post('/animals', [AnimalController::class, 'store']);
-
-// Method PUT
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
-
-// Method DELETE
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+# Route students
+# Method GET
+Route::get('/students', [StudentController::class, 'index']);
+
+# Method POST
+Route::post('/students', [StudentController::class, 'store']);
